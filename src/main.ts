8f8d3b87118fcd3c7b98862deb6792628a4c7885
Ivan Olrov <ivanorlov@Ivans-MacBook-Air.local>
2026-03-04 14:35:50 +0300

    open cors

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,10 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors((req: Request, callback) => {
-    let corsOptions;
-    if (req.url.includes('/api/mock')) {
-      corsOptions = {
-        origin: true,
-        credentials: false,
-      };
-    } else {
-      corsOptions = {
-        origin: process.env.FRONTEND_URL || 'http://localhost:9000',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true,
-      };
-    }
-
-    // Возвращаем примененные настройки
-    callback(null, corsOptions);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   app.setGlobalPrefix('api');
   app.use(cookieParser());
